61dabbfb2d83d35ee29e8e861db8bda557f774fc
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-06 13:07:01 +0100

    validation changes in my models

--- backend/Models/Category.cs
+++ backend/Models/Category.cs
@@ -1,14 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 
-
 namespace backend.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Category Name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Category Name must be between 2 and 50 characters long.")] // min max length
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Category Name can only contain letters and spaces.")] // settings for allowing certain signs
         [Display(Name = "Category Name")]
-        public string? Name { get; set; } = String.Empty;
+        public string? Name { get; set; } = string.Empty;
 
         // Navigation property to Products
         public List<Product>? Products { get; set; }
--- backend/Models/Product.cs
+++ backend/Models/Product.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,16 +8,22 @@ namespace backend.Models
     {
         [Key]
         public int ProductId { get; set; }
+
         [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [RegularExpression(@"^[^!?\s]+$", ErrorMessage = "Title cannot contain ! or ?")] // settings for allowing certain signs
         public string? Title { get; set; }
+
         [Required]
+        [StringLength(1000, MinimumLength = 5)] // set min/max length
         public string? Description { get; set; }
+
         [Required]
         public decimal Price { get; set; }
+
         [Required]
         [Display(Name = "Highlight")]
         public bool IsHighlighted { get; set; }
-
 
         [Display(Name = "Image URL")]
         public string? ImageName { get; set; }
@@ -29,6 +36,7 @@ namespace backend.Models
         public string ImageWebp { get; set; } = String.Empty;
 
         [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Alt Text must be between 2 and 100 characters long.")] // min max length
         [Display(Name = "Alt Text")]
         public string? ImageAlt { get; set; }
 
