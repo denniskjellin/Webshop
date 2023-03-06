027800527038c9c3324ea2afe4c11c35f039ff27
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-06 17:15:45 +0100

    setting up pages and category pages

---
+++ frontend/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quibusdam porro reprehenderit corrupti sed sapiente blanditiis iusto sunt, nihil labore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quibusdam porro reprehenderit corrupti sed sapiente blanditiis iusto sunt, nihil labore.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 1.3rem;
+        font-size: 2.2rem;
+    }
+
+    p {
+        margin: 1.3rem 0;
+    }
+</style>
