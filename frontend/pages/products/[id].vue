317b0db1b5da559536a778f1bdf90b07be221cd2
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-06 17:24:23 +0100

    setting up id pages

---
+++ frontend/pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details Index ID {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem ut facere quia esse incidunt laudantium labore voluptas cupiditate fugit.</p>
+    </div>
+</template>
+
+<script setup>
+const {id} = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
