0dc0fb05930dd1637971d67459330d84c23debf0
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-07 16:07:27 +0100

    installed font-awesome icons

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     // Tailwind  CSS
-    modules: ['@nuxtjs/tailwindcss']
+    modules: ['@nuxtjs/tailwindcss'],
+    css: [
+        '@fortawesome/fontawesome-free/css/all.css',
+        '@fortawesome/fontawesome-free/css/fontawesome.css'
+      ]
 
 })
+
+
