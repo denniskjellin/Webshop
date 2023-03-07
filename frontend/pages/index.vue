6e892d11cd5490520d45cf72b8bbca0e64db26be
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-07 10:32:50 +0100

    smal changes

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,43 +1,39 @@
 <template>
-<div class="container">
-  <h1>Home</h1>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-    <div class="bg-gray-200 p-4">
-      <h2 class="text-xl font-bold mb-2">Box 1</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non eros eget dui ultricies mattis. Donec euismod sem at dui ullamcorper interdum.</p>
+  <section>
+    <h1 class="text-center text-4xl mb-6">Home</h1>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div class="bg-gray-200 p-4">
+        <h2 class="text-2xl font-bold mb-2">Box 1</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non
+          eros eget dui ultricies mattis. Donec euismod sem at dui ullamcorper
+          interdum.
+        </p>
+      </div>
+      <div class="bg-gray-200 p-4">
+        <h2 class="text-2xl font-bold mb-2">Box 2</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non
+          eros eget dui ultricies mattis. Donec euismod sem at dui ullamcorper
+          interdum.
+        </p>
+      </div>
+      <div class="bg-gray-200 p-4">
+        <h2 class="text-2xl font-bold mb-2">Box 3</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non
+          eros eget dui ultricies mattis. Donec euismod sem at dui ullamcorper
+          interdum.
+        </p>
+      </div>
     </div>
-    <div class="bg-gray-200 p-4">
-      <h2 class="text-xl font-bold mb-2">Box 2</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non eros eget dui ultricies mattis. Donec euismod sem at dui ullamcorper interdum.</p>
-    </div>
-    <div class="bg-gray-200 p-4">
-      <h2 class="text-xl font-bold mb-2">Box 3</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non eros eget dui ultricies mattis. Donec euismod sem at dui ullamcorper interdum.</p>
-    </div>
-  </div>
-</div>
-
+  </section>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
-    h2 {
-        margin-bottom: 1.3rem;
-        font-size: 2.2rem;
-    }
-
-    h1 {
-    text-align: center;
-    font-size: 2.2rem;
-    margin-bottom: 1.3rem;
-  }
-
-    p {
-        margin: 1.3rem 0;
-    }
-
-
+p {
+  margin: 1.3rem 0;
+}
 </style>
