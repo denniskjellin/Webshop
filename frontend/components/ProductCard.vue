b9db21c6d9e0b8684f2511df6a2420f34dbb6afe
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-08 12:19:56 +0100

    styling card

--- frontend/components/ProductCard.vue
+++ frontend/components/ProductCard.vue
@@ -1,7 +1,9 @@
 <template>
    <div class="card text-center">
         <img :src="product.imageName" :alt="product.imageAlt" class="thumb">
-        <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
+        <p class="font-bold text-gray-700 m-4 truncate uppercase tracking-widest">{{ product.title }}</p>
+        <p class=" text-gray-600 m-1 truncate">{{ product.description }}</p>
+        <p class=" text-green-700 text-xl font-bold m-2 truncate">${{ product.price }}</p>
         <NuxtLink :to="`/products/${product.id}`">
             <button class="btn-card">View Product</button>
         </NuxtLink>
@@ -14,9 +16,13 @@
 
 <style lang="scss" scoped>
 .thumb {
-    max-height: 200px;
+    max-height: 200px; // maybe change this later
     max-width: 70%;
     margin: 0 auto;
+    margin-top: 1rem;
+
 }
+
+
 
 </style>
