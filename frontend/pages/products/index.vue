f733b3d4d2d853faf52714c67271b539fdd0d712
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-08 17:44:58 +0100

    categiry nav

--- frontend/pages/products/index.vue
+++ frontend/pages/products/index.vue
@@ -1,20 +1,33 @@
 <template>
   <section>
-    <h2 class="text-center text-4xl mb-6 uppercase">trending</h2>
     <!-- Category Menu -->
-    <div class="md:flex items-center">
-      <NuxtLink to="/" class=" text-gray-700 p-4 hover:font-bold hover:underline hover:text-green-700"
-            >Home</NuxtLink
-          >
-          <NuxtLink to="/" class=" text-gray-700 p-4 hover:font-bold hover:underline hover:text-green-700"
-            >Home</NuxtLink
-          >
-          <NuxtLink to="/" class=" text-gray-700 p-4 hover:font-bold hover:underline hover:text-green-700"
-            >Home</NuxtLink
-          >
-          <NuxtLink to="/" class=" text-gray-700 p-4 hover:font-bold hover:underline hover:text-green-700"
-            >Home</NuxtLink
-          >
+    <div class="md:flex items-center border-x-4 border-green-500 mb-10">
+      <div class="flex flex-col items-center justify-center">
+        <NuxtLink to="/" class="text-xl text-gray-700 p-4  hover:text-gray-500">
+          <span class="text-2xl mb-2 font-bold">Amino</span><br>
+          <span class="text-base">Boost your workout with our high quality Amino supplements.</span>
+        </NuxtLink>
+      </div>
+      <div class="flex flex-col items-center justify-center">
+        <NuxtLink to="/" class="text-xl text-gray-700 p-4  hover:text-gray-500">
+          <span class="text-2xl mb-2 font-bold">Preworkout</span><br>
+          <span class="text-base">Get ready to crush your workout with our Preworkout formulas.</span>
+        </NuxtLink>
+      </div>
+      <div class="flex flex-col items-center justify-center">
+        <NuxtLink to="/" class="text-xl text-gray-700 p-4  hover:text-gray-500">
+          <span class="text-2xl mb-2 font-bold">Protein</span><br>
+          <span class="text-base">Fuel your muscles with our high-quality Protein powders.</span>
+        </NuxtLink>
+      </div>
+    </div>
+    <h1 class="text-center text-4xl mb-6 uppercase">Products</h1>
+
+     <!---product cards -->
+     <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div v-for="p in products" class="w-full md:w-auto">
+        <ProductCard :product="p" v-if="p.isHighlighted" />
+      </div>
     </div>
 
     <!---info section -->
@@ -39,6 +52,13 @@
 </template>
 
 <script setup>
+
+
+// fetch the products
+const { data: products } = await useFetch(
+  "https://acciodennis.azurewebsites.net/api/products"
+);
+
 // import layout
 definePageMeta({
   layout: "default",
