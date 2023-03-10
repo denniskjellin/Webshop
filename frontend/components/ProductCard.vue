54ba781a792d2bbef39110a279ddbd8f1f77fa1c
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-10 13:56:51 +0100

    id product

--- frontend/components/ProductCard.vue
+++ frontend/components/ProductCard.vue
@@ -2,13 +2,13 @@
   <section class="card text-center pb-8">
     <img :src="product.imageName" :alt="product.imageAlt" class="thumb" />
     <h2 class="font-bold text-gray-700 mt-4 truncate uppercase tracking-widest">
-      {{ product.title }}
+      {{ product.id }}
     </h2>
 
     <p class="text-green-700 text-xl font-bold mb-5 mt-1 truncate">
       ${{ product.price }}
     </p>
-    <NuxtLink :to="`/products/${product.id}`">
+    <NuxtLink :to="`/products/${product.productId}`">
       <span class="btn-card">View Product</span>
     </NuxtLink>
   </section>
--- frontend/pages/products/[id].vue
+++ frontend/pages/products/[id].vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <p>Product details Index ID {{ id }}</p>
+        <h1>är det här??</h1>
+        <h2>{{ product.title }}</h2>
+        <h2>{{ product.description }}</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem ut facere quia esse incidunt laudantium labore voluptas cupiditate fugit.</p>
     </div>
 </template>
 
 <script setup>
 const {id} = useRoute().params
+const uri = `https://acciodennis.azurewebsites.net/api/products/${id}`
+
+const { data: product } = await useFetch(uri, { key: id  })
 </script>
 
 <style lang="scss" scoped>
