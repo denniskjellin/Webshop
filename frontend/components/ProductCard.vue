7446d6acfe59c3a7f31e327c405e2337ddba02a5
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-10 16:31:40 +0100

    push

--- frontend/components/ProductCard.vue
+++ frontend/components/ProductCard.vue
@@ -5,9 +5,7 @@
       {{ product.id }}
     </h2>
 
-    <p class="text-green-700 text-xl font-bold mb-5 mt-1 truncate">
-      ${{ product.price }}
-    </p>
+    <p class="text-green-700 text-xl font-bold mb-5 mt-1 truncate">{{ product.price }}:-</p>
     <!-- Link to product with id -->
     <NuxtLink :to="`/products/${product.productId}`">
       <span class="btn-card">View Product</span>
@@ -22,7 +20,7 @@ const { product } = defineProps(["product"]);
 
 <style lang="scss" scoped>
 .thumb {
-  max-height: 250px; // maybe change this later
+  max-height: 300px; // maybe change this later
   max-width: 70%;
   margin: 0 auto;
   margin-top: 1rem;
--- frontend/pages/products/[id].vue
+++ frontend/pages/products/[id].vue
@@ -1,21 +1,33 @@
 <template>
-    <div>
-        <h1>är det här??</h1>
-        <h2>{{ product.title }}</h2>
-        <h2>{{ product.description }}</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem ut facere quia esse incidunt laudantium labore voluptas cupiditate fugit.</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div>
+        <img class="w-full" :src="product.imageName" :alt="product.imageAlt"  />
+      </div>
+      <div>
+        <h1 class="text-3xl font-bold mb-4">{{ product.title }}</h1>
+        <p class="text-gray-600 text-lg mb-4">{{ product.description }}</p>
+        <p class="text-2xl font-bold mb-4">{{ product.price }} kr</p>
+        <button class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Add to cart</button>
+      </div>
     </div>
-</template>
-
-<script setup>
-// route id
-const {id} = useRoute().params
-// url and tag on id
-const uri = `https://acciodennis.azurewebsites.net/api/products/${id}`
-// fetch data
-const { data: product } = await useFetch(uri, { key: id  })
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <script setup>
+  definePageMeta({
+    layout: 'products'
+  })
+    // route id
+    const { id } = useRoute().params
+    // url and tag on id
+    const uri = `https://acciodennis.azurewebsites.net/api/products/${id}`
+    // fetch data
+    const { data: product } = await useFetch(uri, { key: id })
+  </script>
+  
+  <style lang="scss" scoped>
+    /* Optional styles for image aspect ratio */
+    .w-full {
+      aspect-ratio: 4/4;
+    }
+  </style>
+  
