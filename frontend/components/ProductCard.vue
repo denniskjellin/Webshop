44617d9dc6d59954ca964c406456b5363bca91c3
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-11 10:53:16 +0100

    improvement

--- frontend/components/ProductCard.vue
+++ frontend/components/ProductCard.vue
@@ -1,4 +1,5 @@
 <template>
+  <NuxtLink :to="`/products/${product.productId}`">
   <section class="card text-center pb-8">
     <img :src="product.imageName" :alt="product.imageAlt" class="thumb" />
     <h2 class="font-bold text-gray-700 mt-4 truncate uppercase tracking-widest">
@@ -7,10 +8,11 @@
 
     <p class="text-green-700 text-xl font-bold mb-5 mt-1 truncate">{{ product.price }}:-</p>
     <!-- Link to product with id -->
-    <NuxtLink :to="`/products/${product.productId}`">
+    
       <span class="btn-card">View Product</span>
-    </NuxtLink>
+    
   </section>
+</NuxtLink>
 </template>
 
 <script setup>
