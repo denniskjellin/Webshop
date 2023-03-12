ffbca046f227748e7b3b033cdb54ffefe35c3683
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-12 13:07:41 +0100

    adding font

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
     // Tailwind  CSS
     modules: ['@nuxtjs/tailwindcss'],
+    app: {
+      head: {
+        title: 'Accio',
+        meta: [
+          {name: 'description', content: 'Accio webshop'},
+        ],
+        link: [
+          {rel: 'font', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Poppins&display=swap'},
+        ]
+      }
+    },
     css: [
         '@fortawesome/fontawesome-free/css/all.css',
         '@fortawesome/fontawesome-free/css/fontawesome.css'
